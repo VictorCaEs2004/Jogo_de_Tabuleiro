e3869f9ea812e64acc56ca93c1a9ca8b3a47fc4d
Paulo Alexandre <paulo.rd.brt@gmail.com>
2025-06-14 18:38:35 -0300

    Painel principal na janela

--- GUI/Janela.java
+++ GUI/Janela.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import java.awt.Color;
 
 public class Janela extends JFrame{
@@ -11,7 +13,17 @@ public class Janela extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //faz com que o programa pare quando a janela é fechada (EXIT termina todas as janelas, DISPOSE termina só a que foi fechada)
         this.setResizable(false); //proíbe o usuário de redimensionar a janela
         this.getContentPane().setBackground(Color.lightGray);
+        this.setLayout(null);
+
+        preparaPainelPrincipal();
+
 
         this.setVisible(true);
+    }
+
+    public void preparaPainelPrincipal(){
+        //JPanel: componente que funciona como container, possuindo outros componentes
+        JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setBounds(0, 0, 1600, 400);
     }
 }
